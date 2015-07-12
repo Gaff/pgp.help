dd6a620146626c96ae7a04adfd7bb7a9722a624e
Matthew Shaylor <github@shaylor.co.uk>
2015-07-12 19:23:32 +0100

    Hash handling in URLs

--- js/ezpgp.js
+++ js/ezpgp.js
@@ -67,9 +67,18 @@
 	//Hooks on startup
 	$(document).ready( function() {
 		//console.log ('document ready - woot!');
+		
+		//redirect to https.
 		if ((window.location.host=='pgp.help' || window.location.host=='gaff.github.io') && window.location.protocol!="https:")
             window.location.protocol = "https";
 
+        
+        if (window.location.hash)
+          $('#tab-' + window.location.hash.substr(1).split('?')[0]).tab('show');
+
+        $('a[data-toggle="tab"]').on('click', function (e) {
+            window.location.hash = $(this).attr('href');
+        });
 
 		onInput('#src', onChangeFrom);
 		onInput('#pgppub', onChangeFrom);
