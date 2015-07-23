c3d41464c7fe5b5fe8fb887c81ea9081977803ea
Matthew Shaylor <github@shaylor.co.uk>
2015-07-23 11:43:31 +0100

    slow and fast fades

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 var app = angular.module('myApp', ['ngAnimate']);
 app.controller('MyController', function($scope) {
+  $scope.test = {'class' : '', 'hide' : false};
 
+  $scope.quickhide = function() { $scope.test.hide = true; };
+  $scope.slowhide = function() { $scope.test.hide = true; $scope.test.class = 'smartfade';};
+  $scope.show = function() { $scope.test.hide = false; $scope.test.class = '';};
 });
