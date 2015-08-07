5a93e4d69bf8620ed20cc7465538338ca5dd7ea0
Matthew Shaylor <github@shaylor.co.uk>
2015-08-07 20:34:49 +0100

    Try again

--- gulpfile.js
+++ gulpfile.js
@@ -186,9 +186,9 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('dist', function() {
+  //I get a bit confused about how dependencies work
+  //But basically only run this after a build.
   runSequence(
-    'build',
-    'test',
     'gh-pages'
   )
 })
